96b843ea95338d84888767db2ec788aa51ebcde5
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-15 00:03:52 +0530

    Update length_of_string.c

--- PROGRAMING_WITH_C/string/length_of_string.c
+++ PROGRAMING_WITH_C/string/length_of_string.c
@@ -11,12 +11,3 @@ int main() {
     printf("Length is: %d\n", c);
     return 0;
 }
-hell o fas[sd]  dssd
-n sdcsdh STDIN_FILENOb sdfkh _DISABLE_MSVC_PRINTF_QUIRKSs STDIN_FILENOsd ds
- sd
-  #include <stdio.h>
-  
-  int main(){
-
-  return 0;
-  }
