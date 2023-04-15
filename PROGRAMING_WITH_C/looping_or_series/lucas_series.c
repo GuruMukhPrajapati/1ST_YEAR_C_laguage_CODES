5bfe3e1a223ca63a30c5257ba61e13144bc91a42
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-15 20:20:14 +0530

    Update lucas_series.c

---
+++ PROGRAMING_WITH_C/looping_or_series/lucas_series.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int x,y,z,num;
+ clrscr();
+ printf("Enter the limit of Lucas number : ");
+ scanf("%d",&num);
+ x=2;
+ y=1;
+ while(num>=x)
+ {
+   printf(" %d",x);
+   z=x+y;
+   x=y;
+   y=z;
+ }
+ getch();
+
+}
