8a418c30a8ced82c7a815e79737bba89d4861425
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-16 20:12:44 +0530

    Update reverse_string.c

--- PROGRAMING_WITH_C/string/reverse_string.c
+++ PROGRAMING_WITH_C/string/reverse_string.c
@@ -12,3 +12,28 @@ int main(){
     }
     return 0;
 }
+  
+=======================================================================================
+// code with out using libarary funcation (strlen) 
+// this is another code worked same 
+=======================================================================================
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char st[100];
+    printf("enter the string : ");
+    scanf("%s", &st);
+   
+    printf("the reverse string is : ");
+    for (i = 0; st[i] != '\0'; i++);
+    
+    for (i = i - 1; i >= 0; i--)
+     {
+        printf("%c", st[i]);
+     }
+    
+    return 0;
+}
