2e1c40099e32e5152e42b32e11d32ad574670f15
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-17 18:33:39 +0530

    Update continoue_statement.c

--- PROGRAMING_WITH_C/looping_or_series/continoue_statement.c
+++ PROGRAMING_WITH_C/looping_or_series/continoue_statement.c
@@ -2,27 +2,21 @@
 
 int main()
 {
-    printf("Hello World\n");
-    int i, age;
-    for (i=0; i<10; i++){
-        printf("%d\nEnter your age\n", i);
-        scanf("%d", &age);
-        // if (age>10)
-        // {
-        //     break;
-        // }
-        if (age>10)
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        if (i == 3)
         {
             continue;
         }
-        printf("we have not come accross any continue statements");
-        printf("we have not come accross any continue statements");
-        printf("we have not come accross any continue statements");
-        printf("we have not come accross any continue statements");
-        printf("Harry is a good boy");
-        
+        printf("%d\n", i);
     }
-    
     return 0;
 }
 
+/*
+output
+1
+2
+4
+5
