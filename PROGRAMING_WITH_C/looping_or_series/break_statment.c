36bf4e4bc5d74708b3542e482e195510cc282719
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-17 18:17:09 +0530

    Create break_statment.c

---
+++ PROGRAMING_WITH_C/looping_or_series/break_statment.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() 
+{ 
+	int i,age; 
+	for(i = 0 ; i < 5 ; i++) \
+	{ 
+
+		printf("Iteration time = %d\nEnter Your Age : ",i ); 
+		scanf("%d",&age); 
+		if (age>10) 
+		{ 
+			break; // Checking Break Statement 
+		} 
+		// if(age<10) 
+		// { continue; } 
+		// printf("Hey Guys\n"); 
+		// printf("This code is printed coz if condition is not satisfied. \n"); 
+		// printf("Checking Continue Statement\n\n"); // Checking Continue Statement 
+	} return 0; 
+} 
