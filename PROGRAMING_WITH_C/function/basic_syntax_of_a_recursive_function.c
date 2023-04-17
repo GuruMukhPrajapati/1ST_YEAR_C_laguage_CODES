70e632c1947428ddb42484f3ec3a4fca48520716
Gourav prajapat <117162170+GuruMukhPrajapati@users.noreply.github.com>
2023-04-17 13:26:02 +0530

    Add files via upload

---
+++ PROGRAMING_WITH_C/function/basic_syntax_of_a_recursive_function.c
@@ -0,0 +1,14 @@
+
+void recursive_fun() //recursive function
+{
+    Base_case; // Stopping Condition
+    
+    recursive_fun(); //recursive call
+}
+
+int main()
+{
+   
+   recursive_fun(); //function call
+    
+}
